6bdca64313bcd6caebb8f695c82b6c9c063fe79f
Alvaro <alvaroperbay97@gmail.com>
2016-02-19 10:43:50 +0100

    Implementado el metodo que permite imprimir por pantalla los productos que tienen un stock por debajo del parametro pasado

--- StockManager.java
+++ StockManager.java
@@ -28,7 +28,7 @@ public class StockManager
     {
         stock.add(item);
     }
-    
+
     /**
      * Receive a delivery of a particular product.
      * Increase the quantity of the product by the given amount.
@@ -37,13 +37,15 @@ public class StockManager
      */
     public void delivery(int id, int amount)
     {
-        for (Product producto : stock) {
-            if (id == producto.getID()){
-                producto.increaseQuantity(amount);
-            }
+        Product producto = findProduct(id);
+        if(producto != null){
+            producto.increaseQuantity(amount);
+        }
+        else{
+            System.out.println("El id indicado no pertenece a ningun producto");
         }
     }
-    
+
     /**
      * Try to find a product in the stock with the given id.
      * @return The identified product, or null if there is none
@@ -51,15 +53,19 @@ public class StockManager
      */
     public Product findProduct(int id)
     {
-        Product numProducto = null;
-        for (Product producto : stock) {
-            if (id == producto.getID()){
-                numProducto = producto;
+        Product producto = null;
+        int index = 0;
+        boolean encontrado = false;
+        while(!encontrado && index < stock.size()) {
+            if (stock.get(index).getID() == id) {
+                producto = stock.get(index);
+                encontrado = true;
             }
+            index++;
         }
-        return numProducto;
+        return producto;
     }
-    
+
     /**
      * Locate a product with the given ID, and return how
      * many of this item are in stock. If the ID does not
@@ -69,13 +75,12 @@ public class StockManager
      */
     public int numberInStock(int id)
     {
-        int numProductos = 0;
-        for (Product producto : stock) {
-            if (id == producto.getID()){
-                numProductos = producto.getQuantity();
-            }
+        int cantidad = 0;
+        Product producto = findProduct(id); 
+        if (producto != null) {
+            cantidad = producto.getQuantity();
         }
-        return numProductos;
+        return cantidad;
     }
 
     /**
@@ -85,6 +90,22 @@ public class StockManager
     {
         for (Product producto : stock) {
             System.out.println(producto);
+        }
+    }
+    
+    /**
+     * Imprime los detalles de todos los productos que tienen un stock
+     * por debajo del parametro pasado
+     */
+    public void underGivenNumberInStock(int topeEnStock) 
+    {
+        for (Product producto : stock) {
+            if (producto.getQuantity() < topeEnStock) {
+                System.out.println(producto);
+            }
+            else {
+                System.out.println("No hay ningun producto que tenga un stock tan bajo");
+            }
         }
     }
 }
